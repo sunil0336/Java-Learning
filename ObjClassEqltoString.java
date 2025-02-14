c01db7ab81a05386a9e7cde9fea31ac4c51702f7
sunil0336 <sunilrathod.0336@gmail.com>
2025-02-14 23:51:56 +0530

    test

--- ObjClassEqltoString.java
+++ ObjClassEqltoString.java
@@ -7,14 +7,40 @@ class Laptop{
     public String toString(){
         return model + " : " + price;
     }
-    public boolean equals(Laptop that){
-        // if(this.model.equals(that.model) && this.price == that.price)
-        //     return true;
-        // else
-        //     return false;
-
-        return this.model.equals(that.model) && this.price == that.price;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((model == null) ? 0 : model.hashCode());
+        result = prime * result + price;
+        return result;
     }
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Laptop other = (Laptop) obj;
+        if (model == null) {
+            if (other.model != null)
+                return false;
+        } else if (!model.equals(other.model))
+            return false;
+        if (price != other.price)
+            return false;
+        return true;
+    }
+    // public boolean equals(Laptop that){
+    //     // if(this.model.equals(that.model) && this.price == that.price)
+    //     //     return true;
+    //     // else
+    //     //     return false;
+
+    //     return this.model.equals(that.model) && this.price == that.price;
+    // }
 }
 
 
