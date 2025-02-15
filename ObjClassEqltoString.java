b4e528c8040eaf01f8627134282f1e0c8fd98544
sunil0336 <sunilrathod.0336@gmail.com>
2025-02-15 23:22:49 +0530

    update

--- ObjClassEqltoString.java
+++ ObjClassEqltoString.java
@@ -33,14 +33,6 @@ class Laptop{
             return false;
         return true;
     }
-    // public boolean equals(Laptop that){
-    //     // if(this.model.equals(that.model) && this.price == that.price)
-    //     //     return true;
-    //     // else
-    //     //     return false;
-
-    //     return this.model.equals(that.model) && this.price == that.price;
-    // }
 }
 
 
