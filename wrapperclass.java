6483c00f98e287e6b586f8bc1379ecbf236ad37b
sunil0336 <sunilrathod.0336@gmail.com>
2025-02-17 23:30:39 +0530

    wrapper class

---
+++ wrapperclass.java
@@ -0,0 +1,11 @@
+public class wrapperclass {
+    public static void main(String[] args) {
+        
+        int num = 7;
+
+        // boxing , here we are stoaring a premitive values in obj
+        // Integer num1 = new Integer(num);  
+        Integer num1 = num;  // autoboxing
+
+    }
+}
