8ec00bafaac9d7a9d24ba225fb445314039eb107
sunil0336 <sunilrathod.0336@gmail.com>
2025-02-20 23:17:09 +0530

    update wrapperclass

--- wrapperclass.java
+++ wrapperclass.java
@@ -7,5 +7,15 @@ public class wrapperclass {
         // Integer num1 = new Integer(num);  
         Integer num1 = num;  // autoboxing
 
+
+        // int num2 = num1.intValue(); // unboxing
+        int num2 = num1; // auto-unboxing 
+
+        System.out.println(num2);
+
+        String str = "5";
+        int num3 = Integer.parseInt(str); // converting string into the integer
+        System.out.println(num3*2);
+
     }
 }
